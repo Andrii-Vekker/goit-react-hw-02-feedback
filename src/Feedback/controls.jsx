ec8d409b00bfbb07d76a9d2b89080a68b04e2fc9
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-09-15 16:15:20 +0300

    add css

--- src/Feedback/controls.jsx
+++ src/Feedback/controls.jsx
@@ -1,11 +1,11 @@
-
+import { ControlsBox, BtnStyles } from "./Controls.styled";
 
 export default function Controls({onIncrement}) {
     return (
-        <>
-            <button type="button" className="btn" name="good" onClick={onIncrement}>Good</button>
-            <button type="button" className="btn" name="neutral" onClick={onIncrement}>Neutral</button>
-            <button type="button" className="btn" name="bad" onClick={onIncrement}> Bad</button>
-        </>
+        <ControlsBox>
+            <BtnStyles type="button" name="good" onClick={onIncrement}>Good</BtnStyles>
+            <BtnStyles type="button" name="neutral" onClick={onIncrement}>Neutral</BtnStyles>
+            <BtnStyles type="button" name="bad" onClick={onIncrement}> Bad</BtnStyles>
+        </ControlsBox>
   );
 };
