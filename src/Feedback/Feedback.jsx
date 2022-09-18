079cafa3bac937a99b9180caa0f6d6edf8c8f9a4
Andrii-Vekker <vekker304@gmail.com>
2022-09-18 09:36:30 +0300

    wqdwqd

--- src/Feedback/Feedback.jsx
+++ src/Feedback/Feedback.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from 'prop-types';
 import Controls from "./controls";
 import Statistic from "./Statistic";
 import Section from "./Section";
@@ -53,4 +54,6 @@ countTotalFeedback() {
       );
     };
 };
+
+  
 
--- src/Feedback/Notifycation.jsx
+++ src/Feedback/Notifycation.jsx
@@ -1,7 +1,12 @@
-
+import PropTypes from 'prop-types';
 
 export default function Notifycation({message}) {
   return (
     <>{message}</>
   );
 };
+
+
+// Notifycation.PropTypes = {
+//   message: PropTypes.string.isRequired
+// };
